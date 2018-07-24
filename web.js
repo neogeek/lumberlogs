21e841e6a6fab4e12a43d6be3f9aa3055986a7bd
Scott Doxey <neogeek0101@gmail.com>
2018-07-24 18:14:06 -0400

    Added createWriteStream to log to local file.

--- web.js
+++ web.js
@@ -1,5 +1,12 @@
 require('with-env')();
 
+const { createWriteStream } = require('fs');
+const { spawn } = require('child_process');
+
+spawn('mkdir', '-p logs'.split(' '));
+
+const wstream = createWriteStream('logs/log.log');
+
 const restify = require('restify');
 
 const server = restify.createServer({
@@ -11,6 +18,7 @@ server.use(restify.plugins.bodyParser());
 server.post('/log', (req, res) => {
     const logString = `${new Date().toLocaleString()} ${req.body}\n`;
     process.stdout.write(logString);
+    wstream.write(logString);
     res.send(204);
 });
 
