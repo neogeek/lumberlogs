3abf01c251a44f7c8156017a8110029219200c09
Scott Doxey <neogeek0101@gmail.com>
2018-07-29 01:25:38 -0400

    Added electron support.

---
+++ app.js
@@ -0,0 +1,30 @@
+require('./web.js');
+
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+const createWindow = () => {
+    win = new BrowserWindow({ width: 800, height: 600 });
+
+    win.loadURL('http://localhost:8000/');
+    win.focus();
+
+    win.on('closed', () => {
+        win = null;
+    });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
