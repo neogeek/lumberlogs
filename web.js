48b60d39a6800e365ee3a5a6c6478e9446fd7e04
Scott Doxey <neogeek0101@gmail.com>
2018-08-04 13:13:10 -0400

    Added cors middleware.

--- web.js
+++ web.js
@@ -7,6 +7,7 @@ const wstream = createWriteStream(`${app.getPath('desktop')}/logs.txt`);
 const PORT = process.env.PORT || 8000;
 
 const restify = require('restify');
+const corsMiddleware = require('restify-cors-middleware');
 
 const server = restify.createServer({
     acceptable: ['text/plain', 'application/x-www-form-urlencoded']
@@ -17,6 +18,13 @@ const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({ server: server.server });
 
 server.use(restify.plugins.bodyParser());
+
+const cors = corsMiddleware({
+    origins: ['*']
+});
+
+server.use(cors.preflight);
+server.use(cors.actual);
 
 server.post('/log', (req, res) => {
     let body = req.body;
