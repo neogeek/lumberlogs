079266ec5b2685cc518b6d405528108f52ae6de0
Scott Doxey <neogeek0101@gmail.com>
2018-08-19 20:42:55 -0400

    Removed code to automatically save log file to desktop.

--- web.js
+++ web.js
@@ -1,15 +1,9 @@
-const { app } = require('electron');
-
-const { createWriteStream } = require('graceful-fs');
-
 const WebSocketServer = require('ws').Server;
 
 const restify = require('restify');
 const corsMiddleware = require('restify-cors-middleware');
 
 const createRestifyServer = (ipAddress, port) => {
-    const wstream = createWriteStream(`${app.getPath('desktop')}/logs.txt`);
-
     const server = restify.createServer({});
 
     const wss = new WebSocketServer({ server: server.server });
@@ -36,7 +30,6 @@ const createRestifyServer = (ipAddress, port) => {
 
         const logString = `${new Date().toLocaleString()}\t${body}\n`;
         process.stdout.write(logString);
-        wstream.write(logString);
         wss.clients.forEach(client => client.send(logString));
         res.send(204);
     });
